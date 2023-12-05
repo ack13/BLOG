5f9b398db1734c7ae1606dd524efb45f47e1c22b
ack13 <apoorva13222@gmail.com>
2023-12-05 14:57:49 +0530

    5th

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const User=require('./models/User');
 // app.use(cors({credentials:true,origin:'http://localhost:3000'}));
 // app.use(cors());
 app.use((req, res, next) => {
-    res.header( "Access-Control-Allow-Origin", "*",);
+    res.header( "Access-Control-Allow-Origin", "http://localhost:3000/",);
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
